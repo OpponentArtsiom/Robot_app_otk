3f57e545058b7751ee42d477d1bb961eee9deb94
OpponentArtsiom <opponentartem@gmail.com>
2025-08-27 10:25:50 +0300

    Удалена папка build из индекса, добавлена в .gitignore

--- build/CMakeFiles/RobotOtkApp_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/RobotOtkApp_autogen"
-  "RobotOtkApp_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/RobotOtkApp_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
